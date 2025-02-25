32821428a78d7122075c88b62213af569a11afac
kendrickweb94 <kendrickzgeorge@gmail.com>
2025-02-25 11:10:54 -0800

    fixed errors

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,37 +1,30 @@
-import {  SignIn } from "@clerk/nextjs";
-import AuthBg from "@/public/imgs/auth-bg.png";
+import { SignIn, SignUp } from "@clerk/nextjs";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
 import PrimaryText from "@/components/ui/primary-text";
 import Link from "next/link";
 
-export { AuthBg };
-
-export default function Page() {
+const Page: React.FC = () => {
   return (
     <main>
       <Navbar />
-      <div className="w-full  mt-20 h-screen flex items-center justify-center">
-       
-        <div className="relative   items-center justify-center flex flex-col h-screen space-y-5 z-30">
-          <SignIn />
-          <div className="flex text-center justify-center flex-wrap items-center gap-2">
-            Don&apos;t have an account? don&apos;t worry 
+      <div className="w-full mt-16 h-screen flex items-center justify-center">
+        <div className="relative items-center justify-center flex flex-col h-screen space-y-5 z-30">
+        <div className="flex text-center justify-center flex-wrap items-center gap-2">
+            Don&apos;t have an account? simply
             <PrimaryText>
-                <Link href={"/sign-up"}>
-                    create an account
-                </Link>
+              <Link href={"/sign-up"}>
+                create an account
+              </Link>
             </PrimaryText>
           </div>
+          <SignIn afterSignOutUrl={"/"} afterSignInUrl="/dashboard" />
+        
         </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
       <Footer />
     </main>
   );
-}
+};
+
+export default Page;
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,36 +1,25 @@
-import {  SignUp } from "@clerk/nextjs";
-import AuthBg from "@/public/imgs/auth-bg.png";
+import { SignUp } from "@clerk/nextjs";
 import Navbar from "@/components/ui/navbar";
 import Footer from "@/components/ui/footer";
 import PrimaryText from "@/components/ui/primary-text";
 import Link from "next/link";
-
-export { AuthBg };
 
 export default function Page() {
   return (
     <main>
       <Navbar />
       <div className="w-full  mt-52 h-screen flex items-center justify-center">
-   
         <div className="relative   items-center justify-center flex flex-col h-screen space-y-5 z-30">
-          <SignUp afterSignOutUrl={"/"} afterSignInUrl="/dashboard" />
           <div className="flex text-center justify-center flex-wrap items-center gap-2">
-            Already have an account? easy peasy just 
+            Already have an account? easy peasy just
             <PrimaryText>
-                <Link href={"/sign-in"}>
-                    login in instead
-                </Link>
+              <Link href={"/sign-in"}>login in instead</Link>
             </PrimaryText>
           </div>
+          <SignUp afterSignOutUrl={"/"} afterSignInUrl="/dashboard" />
         </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+
       <Footer />
     </main>
   );
