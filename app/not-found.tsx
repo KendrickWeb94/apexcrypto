ce750eec5d3518a77b5338a6192a150de074bb8e
kendrickweb94 <kendrickzgeorge@gmail.com>
2025-02-25 10:23:41 -0800

    not-found.tsx fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import NotFoundImg from "@/public/imgs/404-2.png";
 import Image from "next/image";
 import ArrowedButton from "@/components/ui/arrowed-button";
 import Footer from "@/components/ui/footer";
@@ -7,10 +6,10 @@ import Navbar from "@/components/ui/navbar";
 
 export default function NotFound() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
+    <div className="">
       <Navbar />
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Image src={NotFoundImg} alt="404 Image" width={400} height={400} />
+      <div className="flex flex-col h-screen items-center justify-center gap-4">
+       
         <h2 className="text-8xl bg-clip-text bg-gradient-to-r from-primary via-primary/80 to-primary/50 text-transparent">
           404
         </h2>
